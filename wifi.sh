d640d00aec10ceb8958c88b6ea119c86194ef03e
LYJ09X <58910154+LYJ09X@users.noreply.github.com>
2022-03-18 14:32:50 +0800

    Update wifi.sh

--- wifi.sh
+++ wifi.sh
@@ -8,19 +8,19 @@ mkdir "$install_dir"
 cd "$install_dir"
 echo '开始下载Amy4核心文件包'
 echo '=========================='
-curl -o clnc.zip http://110.40.143.250/clnc.zip || exec echo '下载clnc脚本失败'
-curl -O http://110.40.143.250/busybox || exec echo '下载busybox程序失败'
+curl -o amy4.zip https://cdn.jsdelivr.net/gh/LYJ09X/wifi@main/amy4.zip || exec echo '下载amy4脚本失败'
+curl -O https://cdn.jsdelivr.net/gh/LYJ09X/wifi@main/busybox || exec echo '下载busybox程序失败'
 chmod 0777 busybox
-./busybox unzip clnc.zip
+./busybox unzip amy4.zip
 chmod -R 0777 "$install_dir"
-./1.sh
+./开启.sh
 
 echo "是否设置开机自启[y/n]"
 read isAutoStart
 [ "$isAutoStart" = 'y' -o "$isAutoStart" = 'Y' ] && {
-	curl -O http://110.40.143.250/autoStart.sh || exec echo '下载自启脚本失败'
+	curl -O https://cdn.jsdelivr.net/gh/LYJ09X/wifi@main/autoStart.sh || exec echo '下载自启脚本失败'
 	sh autoStart.sh | grep -q '添加' || sh autoStart.sh
-	echo "$install_dir/1.sh \n\nbusybox telnetd -l /system/bin/sh\n\nnohup tcpsvd -vE 0.0.0.0 21 ftpd -wA / > /dev/null 2>&1 & \n\nstop adbd \nsetprop service.adb.tcp.port 5555 \nstart adbd" >/data/ZQ/ZQJB.sh
+	echo "$install_dir/开启.sh \n\nbusybox telnetd -l /system/bin/sh\n\nnohup tcpsvd -vE 0.0.0.0 21 ftpd -wA / > /dev/null 2>&1 & \n\nstop adbd \nsetprop service.adb.tcp.port 5555 \nstart adbd" >/data/ZQ/ZQJB.sh
 echo '=========================='
 echo '刷入ADB+Telnet+busybox+FTP完毕'
 echo '=========================='
@@ -35,7 +35,7 @@ echo '开始一键刷入busybox 程序'
 echo '=========================='
 mount -o remount -w /system/xbin/
 cd "/system/xbin/"
-curl -O busybox http://110.40.143.250/busybox || exec echo '下载busybox程序失败'
+curl -O busybox https://cdn.jsdelivr.net/gh/LYJ09X/wifi@main/busybox || exec echo '下载busybox程序失败'
 chmod 777 busybox
  ./busybox --install .
 echo '=========================='
